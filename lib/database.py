502205f4df0ccb745b849f854d3abef5a4082da7
Raphael Njoroge <raphael.njoroge@student.moringaschool.com>
2025-12-04 22:28:17 +0300

    Add database.py

--- lib/database.py
+++ lib/database.py
@@ -1,10 +1,11 @@
 # lib/database.py
-from sqlalchemy import create_engine # type: ignore
-from sqlalchemy.orm import sessionmaker, declarative_base # type: ignore
+from sqlalchemy import create_engine  # type: ignore
+from sqlalchemy.orm import sessionmaker, declarative_base  # type: ignore
 
 engine = create_engine("sqlite:///school.db")
-Session = sessionmaker(bind=engine)
+
+SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
-session = Session()
+session = SessionLocal()
